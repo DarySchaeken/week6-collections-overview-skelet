5f8154b0aee759668cec928271a0dd081d5a2f20
DarySchaeken <dary.schaeken@student.pxl.be>
2017-11-11 19:40:23 +0100

    3 out of 7
    These exercises are fairly easy and ridiculous, so I don't know whether
    I'll be finishing them or not.

--- src/be/pxl/collections/hashmap/DifficultCalculations.java
+++ src/be/pxl/collections/hashmap/DifficultCalculations.java
@@ -2,20 +2,33 @@ package be.pxl.collections.hashmap;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class DifficultCalculations {
 
     public static void main(String[] args) {
         HashMap<Integer, Long> cache = new HashMap<>();
+        TreeMap<Integer, Long> cache2 = new TreeMap<>();
         // onze berekening faculteit duurt erg lang
         // daarom gaan we, wanneer we een waarde hebben berekend,
         // het resultaat bijhouden in een cache 
         Scanner scanner = new Scanner(System.in);
         int aantal = 0;
         while (aantal < 5) {
-            System.out.println("Geef een getal: ");
+            System.out.println("Geef een getal: ");            
             int getal = scanner.nextInt();
-            long result = faculteit(getal);
+            long result = 0;
+            if(cache.containsKey(getal) || cache2.containsKey(getal)){
+            	//result = cache.get(getal);
+            	result = cache2.get(getal);
+            } else {
+            	result = faculteit(getal);
+            	//cache.put(getal, result);
+            	cache2.put(getal, result);
+            }
+            
+            
+            
             // kan je het berekende resultaat bijhouden in een cache?
             // wanneer je dan hetzelfde getal later nog eens tegenkomt,
             // hoef je het niet opnieuw te berekenen!
@@ -24,6 +37,7 @@ public class DifficultCalculations {
         }
 
         // wat krijg je als je de waarden van de cache in een TreeMap steekt?
+        // Same result?
     
         scanner.close();
     }
